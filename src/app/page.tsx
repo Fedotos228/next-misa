b264857d0af96034e6634e57712238256241aa21
Fedotos228 <tudor.onceanu@gmail.com>
2024-07-11 10:58:45 +0300

    deleting prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Hero from '@/components/blocks/Hero/Hero'
+import Hero from '@/components/blocks/Hero'
 
 export default function Home() {
   return (
---
+++ src/components/elements/Navigation.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const nav = [
+  {
+    href: '#home',
+    label: 'Home'
+  },
+  {
+    href: '#products',
+    label: 'products'
+  },
+  {
+    href: '#gallery',
+    label: 'Gallery'
+  }
+]
+
+export default function Navigation() {
+  return (
+    <div>
+      <ul>
+        .
+        <li></li>
+      </ul>
+    </div>
+  )
+}
